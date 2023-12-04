309b42153e3342746a09b16fd1ba2b3b09cb35bd
Michael Slattery <m2slattery@gmail.com>
2023-12-04 17:38:33 -0500

    Ax command

--- lua/ax/init.lua
+++ lua/ax/init.lua
@@ -3,6 +3,9 @@ local fn = vim.fn
 
 local M = {}
 
+-- FIXME: get rid of redir
+
+-- Configuration options, with defaults
 local config = {}
 
 function M.config(user_config)
@@ -10,27 +13,112 @@ function M.config(user_config)
   return config
 end
 
+function is_git_managed(file)
+  return os.execute('git ls-files --error-unmatch ' .. file .. ' > /dev/null 2>&1') == 0
+end
 
+local function delete_file(file)
+  if is_git_managed(file) then
+    os.execute('git rm -f ' .. file .. ' > /dev/null')
+  else
+    os.remove(file)
+  end
+end
 
+local function remove_from_oldfiles(file)
+  -- local oldfiles = fn.v:oldfiles
+  -- for i, oldfile in ipairs(oldfiles) do
+  --   if oldfile == file then
+  --     table.remove(oldfiles, i)
+  --     break
+  --   end
+  -- end
+  -- vim.v.oldfiles = oldfiles
+end
 
+-- local function remove_from_jumplist(file)
+--   local jumplist = fn.getjumplist()[1]
+--   for i, jump in ipairs(jumplist) do
+--     if jump.file == file then
+--       table.remove(jumplist, i)
+--       break
+--     end
+--   end
+--   fn.setjumplist(jumplist)
+-- end
 
+-- local function remove_from_changelist(file)
+--   local changelist = fn.getchangelist()[1]
+--   for i, change in ipairs(changelist) do
+--     if change.filename == file then
+--       table.remove(changelist, i)
+--       break
+--     end
+--   end
+--   fn.setchangelist(changelist)
+-- end
 
+-- local function remove_global_marks(file)
+--   local marks = fn.getmarklist()
+--   for i, mark in ipairs(marks) do
+--     if mark.file == file then
+--       api.nvim_del_mark(mark.name)
+--     end
+--   end
+-- end
 
+-- local function remove_from_quickfix(file)
+--   local quickfix = fn.getqflist()
+--   for i, item in ipairs(quickfix) do
+--     if item.filename == file then
+--       table.remove(quickfix, i)
+--       break
+--     end
+--   end
+--   fn.setqflist(quickfix)
+-- end
 
+function M.ax()
+  local file = api.nvim_buf_get_name(0)
+  api.nvim_buf_delete(0, {force = true})
+  delete_file(file)
+  -- remove_from_oldfiles(file)
+  -- remove_from_jumplist(file)
+  -- remove_from_changelist(file)
+  -- remove_global_marks(file)
+  -- remove_from_quickfix(file)
+end
 
 function M.setup(setup_config)
   setup_config = setup_config or {}
   M.config(setup_config)
+  -- FIXME: args
+  vim.api.nvim_create_user_command('Ax', function(args)
+    M.ax()
+  end, { nargs = "*" })
   return M
 end
 
+-- leak internals for testing purposes
+-- Do not include in documentation.
 function M.leak()
+  M.is_git_managed = is_git_managed
+  M.delete_file = delete_file
+  M.remove_from_oldfiles = remove_from_oldfiles
+  -- M.remove_from_jumplist = remove_from_jumplist
+  -- M.remove_from_changelist = remove_from_changelist
+  -- M.remove_global_marks = remove_global_marks
+  -- M.remove_from_quickfix = remove_from_quickfix
   return M
 end
 
+-- Force reload of plugin.  Useful during development.
+-- Do not include in documentation.
 function M.reload()
   package.loaded['ax'] = nil
-  return require('ax').setup(config)
+  local m = require('ax')
+  m.setup(config)
+  return m
 end
 
 return M
--- test/ax_spec.lua
+++ test/ax_spec.lua
@@ -1,23 +1,67 @@
 local ax = require('ax')
 
 describe('ax.nvim', function()
+  -- This test is run by plenary and make
+
+  local temp_file_path
+  local buffer_reference
 
   before_each(function()
     ax.leak()
     ax.setup({})
+
+    -- open a new buffer with a new temporary file and save it.
+    vim.cmd('enew')
+    temp_file_path = vim.fn.tempname()
+    vim.cmd('edit ' .. temp_file_path)
+    vim.cmd('write')
+    buffer_reference = vim.api.nvim_get_current_buf() -- Save a reference to the buffer
   end)
 
-  it('ai', function()
-    assert.are.same("7", "7")
+  after_each(function()
+    if temp_file_path and vim.fn.filereadable(temp_file_path) == 1 then
+      os.remove(temp_file_path)
+    end
   end)
 
+  it('sanity', function()
+    assert.are.same( buffer_reference, vim.api.nvim_get_current_buf())
+    assert(vim.fn.filereadable(temp_file_path) == 1, "Temporary file must exist")
+  end)
 
+  it('is_git_managed', function()
+    assert( ax.is_git_managed ('Makefile') , "Git managed" )
+  end)
 
+  it('regular delete_file', function()
+    os.execute('touch normal_file.txt')
+    -- sanity check
+    assert( not ax.is_git_managed ('normal_file.txt'))
 
+    ax.delete_file('normal_file.txt')
 
+    assert(vim.fn.filereadable('normal_file.txt') == 0, "Temporary file must not exist")
+  end)
 
+  it('git delete_file', function()
+    os.execute('touch git_file.txt')
+    os.execute('git add git_file.txt')
+    -- sanity check
+    assert( ax.is_git_managed ('git_file.txt'))
 
+    ax.delete_file('git_file.txt')
 
+    assert( not ax.is_git_managed ('git_file.txt'))
+    assert(vim.fn.filereadable('git_file.txt') == 0, "Temporary file must not exist")
+  end)
 
+  it('Ax command', function()
+    assert(vim.fn.filereadable(temp_file_path) == 1, "Temporary file must exist")
 
+    vim.api.nvim_command('Ax')
+
+    assert(vim.fn.filereadable(temp_file_path) == 0, "Temporary file not must exist")
+  end)
+  -- TODO: push
+  -- TODO: automate test generation
 end)
