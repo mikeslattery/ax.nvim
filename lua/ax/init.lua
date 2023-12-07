b0cc444e03f140ab0c0da0d561c51200e8b781ea
Michael Slattery <m2slattery@gmail.com>
2023-12-07 10:42:55 -0500

    Alpha functionality

--- lua/ax/init.lua
+++ lua/ax/init.lua
@@ -1,5 +1,6 @@
 local api = vim.api
 local fn = vim.fn
+local v = vim.v
 
 local M = {}
 
@@ -24,67 +25,75 @@ local function delete_file(file)
 end
 
 local function remove_from_oldfiles(file)
-  -- local oldfiles = fn.v:oldfiles
-  -- for i, oldfile in ipairs(oldfiles) do
-  --   if oldfile == file then
-  --     table.remove(oldfiles, i)
-  --     break
-  --   end
-  -- end
-  -- vim.v.oldfiles = oldfiles
+  local oldfiles = v.oldfiles
+  for i, oldfile in ipairs(oldfiles) do
+    if oldfile == file then
+      table.remove(oldfiles, i)
+      vim.cmd('call remove(v:oldfiles, ' .. (i - 1) .. ')')
+      break
+    end
+  end
 end
 
--- local function remove_from_jumplist(file)
---   local jumplist = fn.getjumplist()[1]
---   for i, jump in ipairs(jumplist) do
---     if jump.file == file then
---       table.remove(jumplist, i)
---       break
---     end
---   end
---   fn.setjumplist(jumplist)
--- end
+local function remove_from_jumplist(file)
+  local jumplist = fn.getjumplist()[1]
+  for i, jump in ipairs(jumplist) do
+    if jump.file == file then
+      table.remove(jumplist, i)
+      vim.cmd('call remove(getjumplist()[1], ' .. (i - 1) .. ')')
+    end
+  end
+end
 
--- local function remove_from_changelist(file)
---   local changelist = fn.getchangelist()[1]
---   for i, change in ipairs(changelist) do
---     if change.filename == file then
---       table.remove(changelist, i)
---       break
---     end
---   end
---   fn.setchangelist(changelist)
--- end
+local function remove_from_changelist(file)
+  local changelist = fn.getchangelist()[1]
+  for i, change in ipairs(changelist) do
+    if change.filename == file then
+      vim.cmd('call remove(getchangelist()[1], ' .. (i - 1) .. ')')
+    end
+  end
+end
 
--- local function remove_global_marks(file)
---   local marks = fn.getmarklist()
---   for i, mark in ipairs(marks) do
---     if mark.file == file then
---       api.nvim_del_mark(mark.name)
---     end
---   end
--- end
+local function remove_global_marks(file)
+  local marks = fn.getmarklist()
+  for i, mark in ipairs(marks) do
+    if mark.file == file then
+      api.nvim_del_mark(string.sub(mark.mark, -1))
+    end
+  end
+end
 
--- local function remove_from_quickfix(file)
---   local quickfix = fn.getqflist()
---   for i, item in ipairs(quickfix) do
---     if item.filename == file then
---       table.remove(quickfix, i)
---       break
---     end
---   end
---   fn.setqflist(quickfix)
--- end
+local function remove_from_quickfix(file)
+  local quickfix = fn.getqflist()
+  for i, item in ipairs(quickfix) do
+    if item.filename == file then
+      table.remove(quickfix, i)
+    end
+  end
+  fn.setqflist(quickfix)
+end
+
+local function remove_from_loclist(file)
+  local current_win = vim.api.nvim_get_current_win()
+  local loclist = fn.getloclist(current_win)
+  for i, item in ipairs(loclist) do
+    if item.filename == file then
+      table.remove(loclist, i)
+    end
+  end
+  fn.setloclist(current_win, loclist)
+end
 
 function M.ax()
   local file = api.nvim_buf_get_name(0)
   api.nvim_buf_delete(0, {force = true})
   delete_file(file)
-  -- remove_from_oldfiles(file)
-  -- remove_from_jumplist(file)
-  -- remove_from_changelist(file)
-  -- remove_global_marks(file)
-  -- remove_from_quickfix(file)
+  remove_from_oldfiles(file)
+  remove_from_jumplist(file)
+  remove_from_changelist(file)
+  remove_global_marks(file)
+  remove_from_quickfix(file)
+  remove_from_loclist(file)
 end
 
 function M.setup(setup_config)
@@ -102,10 +111,11 @@ function M.leak()
   M.is_git_managed = is_git_managed
   M.delete_file = delete_file
   M.remove_from_oldfiles = remove_from_oldfiles
-  -- M.remove_from_jumplist = remove_from_jumplist
-  -- M.remove_from_changelist = remove_from_changelist
-  -- M.remove_global_marks = remove_global_marks
-  -- M.remove_from_quickfix = remove_from_quickfix
+  M.remove_from_jumplist = remove_from_jumplist
+  M.remove_from_changelist = remove_from_changelist
+  M.remove_global_marks = remove_global_marks
+  M.remove_from_quickfix = remove_from_quickfix
+  M.remove_from_loclist = remove_from_quickfix
   return M
 end
 
--- test/ax_spec.lua
+++ test/ax_spec.lua
@@ -6,6 +6,10 @@ describe('ax.nvim', function()
   local temp_file_path
   local buffer_reference
 
+  -- Let's not modify real data
+  vim.opt.shadafile = 'test/main.shada'
+  vim.v.this_session = 'test/Session.vim'
+
   before_each(function()
     ax.leak()
     ax.setup({})
@@ -14,8 +18,10 @@ describe('ax.nvim', function()
     vim.cmd('enew')
     temp_file_path = vim.fn.tempname()
     vim.cmd('edit ' .. temp_file_path)
+    buffer_reference = vim.api.nvim_get_current_buf()
+    -- append a single line to the buffer
+    vim.api.nvim_buf_set_lines(0, -1, -1, false, {"Test line"})
     vim.cmd('write')
-    buffer_reference = vim.api.nvim_get_current_buf() -- Save a reference to the buffer
   end)
 
   after_each(function()
@@ -34,32 +40,97 @@ describe('ax.nvim', function()
   end)
 
   it('regular delete_file', function()
+    -- given
     os.execute('touch normal_file.txt')
     -- sanity check
     assert( not ax.is_git_managed ('normal_file.txt'))
 
+    -- when
     ax.delete_file('normal_file.txt')
 
+    -- then
     assert(vim.fn.filereadable('normal_file.txt') == 0, "Temporary file must not exist")
   end)
 
   it('git delete_file', function()
+    -- given
     os.execute('touch git_file.txt')
     os.execute('git add git_file.txt')
     -- sanity check
     assert( ax.is_git_managed ('git_file.txt'))
 
+    -- when
     ax.delete_file('git_file.txt')
 
+    -- then
     assert( not ax.is_git_managed ('git_file.txt'))
     assert(vim.fn.filereadable('git_file.txt') == 0, "Temporary file must not exist")
   end)
 
   it('Ax command', function()
+    -- given
     assert(vim.fn.filereadable(temp_file_path) == 1, "Temporary file must exist")
 
+    -- when
     vim.api.nvim_command('Ax')
 
+    -- then
     assert(vim.fn.filereadable(temp_file_path) == 0, "Temporary file not must exist")
+  end)
+
+  it('remove_from_jumplist', function()
+    -- given
+    vim.cmd('normal m\'') -- Jump back to the mark to ensure the temp file is in the jumplist
+
+    -- when
+    ax.remove_from_jumplist(temp_file_path)
+
+    -- then
+    local jumplist = vim.fn.getjumplist()[1]
+    for _, jump in ipairs(jumplist) do
+      assert(jump.file ~= temp_file_path, "File should be removed from jumplist")
+    end
+  end)
+
+  it('remove_from_changelist', function()
+    -- given
+    vim.api.nvim_buf_set_lines(0, -1, -1, false, {"Another test line"}) -- Make a change to add to the changelist
+
+    -- when
+    ax.remove_from_changelist(temp_file_path)
+
+    -- then
+    local changelist = vim.fn.getchangelist()[1]
+    for _, change in ipairs(changelist) do
+      assert(change.filename ~= temp_file_path, "File should be removed from changelist")
+    end
+  end)
+
+  it('remove_global_marks', function()
+    -- given
+    vim.cmd('normal mZ') -- Add global mark Z to line in temp_file_path
+
+    -- when
+    ax.remove_global_marks(temp_file_path)
+
+    -- then
+    local marks = vim.fn.getmarklist()
+    for _, mark in ipairs(marks) do
+      assert(mark.file ~= temp_file_path, "File should be removed from global marks")
+    end
+  end)
+
+  it('remove_from_quickfix', function()
+    -- given
+    vim.fn.setqflist({{filename = temp_file_path, lnum = 1, col = 1, text = "Test quickfix entry"}}) -- Add quickfix entry that references file temp_file_path
+
+    -- when
+    ax.remove_from_quickfix(temp_file_path)
+
+    -- then
+    local quickfix = vim.fn.getqflist()
+    for _, item in ipairs(quickfix) do
+      assert(item.filename ~= temp_file_path, "File should be removed from quickfix")
+    end
   end)
 end)
